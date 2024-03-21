873c59677bcbe1751a1a4c6867cc9c8b5dcd2b3b
Oreoluwa <awuloero13@gmail.com>
2024-03-21 15:14:31 +0100

    :sparkles: updates

--- app/(course)/course/[courseId]/chapter/[chapterId]/_components/editor.tsx
+++ app/(course)/course/[courseId]/chapter/[chapterId]/_components/editor.tsx
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.snow.css";
 
 export default function Editor() {
   const [value, setValue] = useState("");
 
-  return (
-    <ReactQuill
-      placeholder="Take note ......"
-      theme="snow"
-      className="h-96 bg-white rounded-lg w-full"
-      value={value}
-      onChange={setValue}
-    />
-  );
+  return <div>sdpiofs</div>;
 }
